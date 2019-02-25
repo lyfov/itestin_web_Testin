dde102e179f312d34a3103ae57d709ce7ac7ccf4
chenhaodongformac <1615385915@qq.com>
2019-02-25 18:12:50 +0800

    格式修改完成

--- runner.py
+++ runner.py
@@ -10,7 +10,7 @@ filename1 = os.getcwd()+"\\"+"Reports\\"
 print(filename1)
 filename = filename1+ time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))
 print(filename)
-fp =open(filename+"Report.html","wb"              )
+fp =open(filename+"Report.html","wb")
 runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title="report", description="fuck")
 runner.run(suite)
 
