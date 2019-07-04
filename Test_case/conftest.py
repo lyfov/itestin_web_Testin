3d9398506581439802ae857f8acfb1d8e2fcecb6
chenhaodong <chenhaodong@testin.cn>
2019-07-04 17:24:44 +0800

    完成30条用例
    Signed-off-by: chenhaodong <chenhaodong@testin.cn>

--- Test_case/conftest.py
+++ Test_case/conftest.py
@@ -1,8 +1,9 @@
-from selenium import webdriver
-
-import pytest
 import os
 import sys
+
+import pytest
+from selenium import webdriver
+
 sys.path.append((os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))))
 @pytest.fixture(scope="function")
 def driver():
@@ -12,6 +13,11 @@ def driver():
         chrome_options.add_argument('--disable-gpu')
         print("========================================")
         driver = webdriver.Chrome(chrome_options=chrome_options)
+
+        # driver =webdriver.Remote(
+        # command_executor="http://10.32.25.59:5555/wd/hub",
+        # desired_capabilities={"browserName": "chrome"}
+        # )
         yield driver
         # driver.save_screenshot("../picture.png")
         # file = open('../test.png', 'rb').read()
